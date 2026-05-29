f7234e3eddee6f3fb8fea3c9dc7a316b9bfed6d3
IsmaelParedes2008 <iparedesn2@unemi.edu.ec>
2026-05-29 16:36:20 -0500

    primer commit

---
+++ ComponentePractico1/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Taller del Componente Practico en REACT </h1>
+      <p>En una nueva y espectacular aventra en REACt</p>
+      <h2>Con: Paredes Niles Ismael Issac</h2>
+    </>
+  )
+}
+
+export default App
